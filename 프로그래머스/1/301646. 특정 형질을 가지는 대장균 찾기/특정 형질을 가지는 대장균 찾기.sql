e8333ed7fc7b9acf92a56b6b30fb00143949da25
Daun Jeong <109944763+daunJJ@users.noreply.github.com>
2024-06-23 16:38:52 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,10 +1,12 @@
 SELECT 
     COUNT(*) as COUNT
 FROM 
+    # 이진수 계산법에 따라 형질 유무를 나타내는 칼럼을 계산 
     (SELECT
         (CASE WHEN GENOTYPE % 2 = 1 THEN 1 ELSE 0 END) as TYPE1,
         (CASE WHEN GENOTYPE DIV 2 % 2 = 1 THEN 1 ELSE 0 END) as TYPE2,
         (CASE WHEN GENOTYPE DIV 2 DIV 2 % 2 = 1 THEN 1 ELSE 0 END) as TYPE3,
         (CASE WHEN GENOTYPE DIV 2 DIV 2 DIV 2 % 2 = 1 THEN 1 ELSE 0 END) as TYPE4
     FROM ECOLI_DATA) AS SUB
+# 조건에 맞는 형질을 가지는 대장균 행만 추출 
 WHERE TYPE2 = 0 and (TYPE1 = 1 OR TYPE3 = 1);
