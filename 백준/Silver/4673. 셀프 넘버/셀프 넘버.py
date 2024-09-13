8e1f16db5124bc673d4e5859c951c21bc7dc4a70
Daun Jeong <109944763+daunJJ@users.noreply.github.com>
2024-09-13 21:08:56 +0900

    [Silver V] Title: 셀프 넘버, Time: 540 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,10 +1,8 @@
 not_self_num = []  # NOT 셀프넘버 모음
 
-for i in range(1,10000):
-  if i not in not_self_num:  #  NOT 셀프넘버 모음에 없음 -> 셀프넘버라는 뜻 
-    print(i)
-  make_num = i  # 생성: d(n)에서 n 더하기 
-  n = str(i)  # 각 자리수를 더하기 위해 문자열로 변환하여 인덱싱 
-  for j in range(len(n)):
-    make_num += int(n[j])  # 생성: d(n)에서 각 자리수 더하기 
-  not_self_num.append(make_num)   # 생성된 수를 NOT 셀프넘버 모음에 추가 
+for num in range(1,10000):
+  if num not in not_self_num:  #  NOT 셀프넘버 모음에 없음 -> 셀프넘버라는 뜻 
+    print(num)
+  for each_num in str(num):  # 생성: d(n)에서 n 더하기
+    num += int(each_num)   # 각 자리수를 더하기 위해 문자열로 변환하여 인덱싱 
+  not_self_num.append(num)   # 생성된 수를 NOT 셀프넘버 모음에 추가 
