2cd139b389623c0af03d82320a587155c18ffa50
Daun Jeong <109944763+daunJJ@users.noreply.github.com>
2024-09-14 18:18:33 +0900

    [Silver I] Title: 회의실 배정, Time: 2604 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,24 +1,21 @@
 N = int(input())
-meet_time = []; start = []; end = []
-# 회의 시작시간, 종료시간을 tuple 형태로 리턴
-for _ in range(N):
-    meet_time.append(tuple(map(int,input().split())))
 
-# 종료 시간을 기준으로 정렬
-# 끝나는 시간이 빠른 회의를 계속 선정 -> 그리디
-meet_time.sort(key=lambda meet_time: (meet_time[1], meet_time[0]))
+# N개의 회의에 대한 (시작 시간, 종료 시간) 받기
+meeting = []
+for i in range(N):
+  s, e = map(int, input().split())
+  meeting.append((s, e))
 
-# cnt: 회의 최대 개수
-# min_end: 초기 상태에서 가장 빨리 끝나는 시간
-cnt = 1; min_end = meet_time[0][1]
-# 이미 회의 한 개는 진행
-for s_e in meet_time[1:]:
-# 현재 종료 시간보다 빠른 경우, 해당 회의 진행 못함
-    if s_e[0] < min_end:
-        pass
-    else:
-# 현재 종료 시간보다 시작 시간이 늦은 경우, 해당 회의 진행 가능
-    # 최소 종료 시간, 회의 최대 개수 업데이트
-        min_end = s_e[1]
-        cnt += 1
+# 회의 종료 시간이 빠른 것부터 배정할 것임 -> 그리디 (현 시점의 최적 찾기)
+meeting.sort(key=lambda x: (x[1], x[0]))
+
+cnt = 0  # 배정된 회의 세기
+end_time = 0  # 종료 시간 초기화 
+
+# 순차적으로 현 시점에서 종료 시간이 빠른 것부터 배정 
+for i in range(len(meeting)):
+  if meeting[i][0] >= end_time:  # 다음 회의의 시작 시간 > 전 회의의 종료 시간 이면 배정
+    end_time = meeting[i][1]  # 해당 회의의 종료 시간 가져오기 
+    cnt += 1  # 배정된 회의 + 1
+  
 print(cnt)
